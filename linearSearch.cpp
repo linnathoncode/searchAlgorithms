7b72a385e705dced230075ca6c1995d58c637ee6
Furkan Göçük <112954805+linnathoncode@users.noreply.github.com>
2023-12-30 13:45:37 +0300

    Update linearSearch.cpp

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -4,13 +4,9 @@ using namespace std;
 
 int linearSearch(int arr[], int size, int x)
 {
-	for(int i = 0; i < size; i++)
-	{
-		if(arr[i] == x)
-		{
-			return i;
-		}
-	}
+	for(int i = 0; i < size; i++) 
+		if(arr[i] == x)return i;
+		
 	return -1;
 }
 
